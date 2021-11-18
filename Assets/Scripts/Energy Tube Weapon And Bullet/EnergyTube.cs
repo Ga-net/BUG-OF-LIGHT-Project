c7cf8a190d59bbed22f88b4583114e999f75412f
Ga-net <81149008+Ga-net@users.noreply.github.com>
2021-11-18 22:24:51 +0200

    Weather System And Textures

---
+++ Assets/Scripts/Energy Tube Weapon And Bullet/EnergyTube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyTube : MonoBehaviour
+{
+    public bool IsBlue;
+    public bool IsYellow;
+    public int EnergyAmount;
+    public float ThrowingForc;
+
+
+    public void TakeEnergy(int EnergyAmount)
+    {
+        EnergyAmount--;
+    }
+
+
+    bool GetThrowed;
+    void ThrowTheTube()
+    {
+        if(!GetThrowed)
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(transform.right* ThrowingForc, transform.position+new Vector3(Random.Range(0.1f, 0.5f), Random.Range(0.1f, 0.5f), Random.Range(0.1f, 0.5f)), ForceMode.Acceleration);
+            GetThrowed = true;
+        }
+    }
+
+
+    private void Update()
+    {
+        if(EnergyAmount <=0)
+        {
+            ThrowTheTube();
+        }
+    }
+
+}
